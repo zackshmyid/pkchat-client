f1d338cbd49070f41762e47072ddacc71047408f
andhra21231 <andhra.rifqi@gmail.com>
2021-12-26 19:42:04 +0700

    add profiler handler

---
+++ client/src/main/java/com/messages/User.java
@@ -0,0 +1,36 @@
+package com.messages;
+
+import jdk.internal.org.jline.utils.Status;
+
+import java.io.Serializable;
+
+/**
+ * Create By : Zack_
+ * Last Update : 26/12/2021
+ */
+public class User implements Serializable {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    String name;
+
+    public void getPicture(String picture){
+        this.picture = picture;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    Status status;
+    String picture;
+}
