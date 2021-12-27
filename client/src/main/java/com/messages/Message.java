61cd79744d02806a7e43fd90fbf2e3e8457ca6c7
andhra21231 <andhra.rifqi@gmail.com>
2021-12-27 09:21:21 +0700

    add Message Handler (Blob And Message Type First)

---
+++ client/src/main/java/com/messages/Message.java
@@ -0,0 +1,12 @@
+package com.messages;
+
+import java.awt.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Message implements Serializable{
+    private String name;
+    private MessageType type;
+
+}
