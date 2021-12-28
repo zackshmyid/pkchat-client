b92896c18b019be5d0b0ab1870cd980ff5e54519
andhra21231 <andhra.rifqi@gmail.com>
2021-12-28 18:53:34 +0700

    Fix False Import (Dumb)

--- client/src/main/java/com/client/login/MainLauncher.java
+++ client/src/main/java/com/client/login/MainLauncher.java
@@ -2,6 +2,7 @@ package com.client.login;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
